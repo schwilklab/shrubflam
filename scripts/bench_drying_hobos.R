3cb5a5be92da59336825de6eb2f238419968bf83
Azaj Mahmud <azmahmud@ttu.edu>
2023-02-03 21:12:09 -0600

    Organized the hobo_bench_drying
    Combined the hobo_bench_drying.R scripts from summer 2021 and summer
    2022 in a single script

--- scripts/bench_drying_hobos.R
+++ scripts/bench_drying_hobos.R
@@ -3,6 +3,7 @@
 ## script to read and clean up hobo data 
 ## for temperature, relative humidity and dew point data 
 ## measured during drying
+## Summer 2021 and summer 2022
 
 library(dplyr)
 
@@ -27,8 +28,30 @@ concat_hobo_files <- function(filelist){
   return(r)
 }
 
+
+###########################################################################
+# Summer 2021
+#############################################################################
+
+hobo_bench_drying <- concat_hobo_files(list.files("../data/year_2021/hobo_data_during_bench_drying",
+                                                  full.names = TRUE,recursive = TRUE,
+                                                  pattern="bench.drying.*csv"))
+
+
+
+# Summary of weather data
+
+bench_drying_summary <- hobo_bench_drying %>%
+  select(1:4)%>%
+  na.omit()%>%
+  summarise(mean.temp=mean(temp),max.temp=max(temp),min.temp=min(temp),
+            mean.rh=mean(rh),max.rh=max(rh),min.rh=min(rh),
+            mean.dewpt=mean(dewpt),max.dewpt=max(dewpt),min.dewpt=min(dewpt))
+
+
+
 ######################################################################################################
-# Reading all the files at a time
+# Reading all the files at a time, summer 2022
 ######################################################################################################
 
 hobo_bench_drying_2022 <- concat_hobo_files(list.files("../data/year_2022/sample_drying_hobos_2022",
@@ -60,5 +83,8 @@ bench_drying_summary_2022 <- hobo_bench_drying_2022 %>%
 # Cleaning the environment
 ############################################################################################################
 
-rm("read_hobo_file","concat_hobo_files","hobo_bench_drying_2022")
+rm("read_hobo_file","concat_hobo_files","hobo_bench_drying", "hobo_bench_drying_2022")
+
+
+
 
