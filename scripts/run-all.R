2e511ecfe60dd410f0c08f1b6a15e1f803a829db
Dylan W. Schwilk <dylan@schwilk.org>
2023-02-09 22:22:56 -0600

    Working run-all.R
    Reads and conducts analyses on all 2022 data. Produces figures in results folder
    
    This should be reasonable basis now for working on final publication quality
    figures.

--- scripts/run-all.R
+++ scripts/run-all.R
@@ -8,6 +8,20 @@
 # This steering script sources all of the non stand-lone code below to read in
 # data, conduct PCAs, fit models, build tables and figures.
 
+## DWS: Note: 2021 data is not read right now for the reasons explained below:
+
+## This can't be run yet ebcause it overwrites the data file names used by
+## read_data.R for 2022 data. So this needs to be fixed if the 2021 data is
+## used. Same applies to 2021 hobo data
+
+# source("./scripts/read_data_2021.R")
+# source("./scripts/read_hobos_2021.R") 
+
+## DWS: The read data and read hobo data scripts 2021 have a lot of repeated code.
+
+
+## Code below for 2022 data all runs cleanly
+
 # read, clean and merged all the morphological and flammability traits data.
 # Produces "alldata_2022"
 source("./scripts/read_data.R")
@@ -16,14 +30,13 @@ source("./scripts/read_data.R")
 # "hobos_wider_2022"
 
 DATA_CACHE_DIR <- "./results/tempdata"
-# source("./scripts/read_hobos.R")  ## Do this once
-#after running the above once, just read saved data:
-hobos_wider_2022 <- readRDS(file.path(DATA_CACHE_DIR, "hobos_wider_2022"))
+source("./scripts/read_hobos.R")  ## Do this once
+## After running the above once, just read saved data:
+# hobos_wider_2022 <- readRDS(file.path(DATA_CACHE_DIR, "hobos_wider_2022"))
 
 # run PCAs requires alldata_2022 and hobos_wider_2022 to exist:
 source("./scripts/flam_pca.R") 
 source("./scripts/analysis.R")
 
-
 # figures
-
+source("./scripts/ms_figures.R")
