c300332623717d92cab72dd7c382abc6a610f53b
Azaj Mahmud <azmahmud@ttu.edu>
2023-04-12 19:09:27 -0500

    Silenced herbivore stuff and add supplimentary
    Silenced all the script in run_all.R script from the second chapter and
    add the supplementary_analysis_2022.R script. Now run_all.R will
    reproduce all the results as well as all the supplimentary
    informationfor manuscript.

--- scripts/run-all.R
+++ scripts/run-all.R
@@ -38,16 +38,18 @@ source("./scripts/read_hobos.R")  ## Do this once
 source("./scripts/flam_pca.R") 
 source("./scripts/analysis.R")
 
-
 # run alldata from the 2021
-source("./scripts/read_data_2021.R")
+#source("./scripts/read_data_2021.R")
 
 # read hobos data for 2021
-source("./scripts/hobo_flam_pca_2021.R")
+#source("./scripts/hobo_flam_pca_2021.R")
 
 # read herbivore analysis script
 
-source("./scripts/herbivore_analysis.R")
+#source("./scripts/herbivore_analysis.R")
 
 # figures
 source("./scripts/ms_figures.R")
+
+# Supplimentary info
+source("./scripts/supplementary_analysis_2022.R")
