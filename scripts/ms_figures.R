94706b4b1352e1e33001d4ccf275bc8388711971
Azaj Mahmud <azmahmud@ttu.edu>
2024-04-13 20:02:05 -0500

    fixed the plots
    Backtransformed the zscore of predictors and plotted with raw data with model intercept and slope, fixed all the minor stuff.

--- scripts/ms_figures.R
+++ scripts/ms_figures.R
@@ -20,24 +20,10 @@ source("./scripts/ggplot_theme.R")
 # Selecting scaled variables from model data 
 ###############################################################################
 
-dim(model_data)
-
-juniperus_data <- model_data %>%
-  filter(analysis_group == "Juniperus") %>%
-  dplyr::select(total_dry_mass_g, leaf_length_per_leaflet,
-                leaf_moisture_content, degsec_100, analysis_group)
-
-mean_juniperus <- juniperus_data %>%
-  group_by(analysis_group) %>%
-  summarize(total_dry_mass_g = mean(total_dry_mass_g),
-            leaf_length_per_leaflet = mean(leaf_length_per_leaflet),
-            leaf_moisture_content = mean(leaf_moisture_content),
-            degsec_100 = mean(degsec_100))
 
 best_degsec_plot_data <- model_data %>% 
-  filter(analysis_group != "Juniperus") %>%
   dplyr::select(total_dry_mass_g, leaf_length_per_leaflet,
-                leaf_moisture_content, degsec_100, analysis_group)
+                leaf_moisture_content, degsec_100, display_name)
 
 dim(best_degsec_plot_data)
 
@@ -47,12 +33,38 @@ dim(best_degsec_plot_data)
 
 
 degsec_by_group <- model_data %>% 
-  filter(analysis_group != "Juniperus") %>%
-  group_by(analysis_group) %>%
+  group_by(display_name) %>%
   summarize(total_dry_mass_g = mean(total_dry_mass_g),
             leaf_length_per_leaflet = mean(leaf_length_per_leaflet),
             leaf_moisture_content = mean(leaf_moisture_content),
             degsec_100 = mean(degsec_100))
+
+degsec_by_group_by_raw_data <- final_data %>% 
+  group_by(display_name) %>%
+  summarize(total_dry_mass_g = mean(total_dry_mass_g),
+            leaf_length_per_leaflet = mean(leaf_length_per_leaflet),
+            leaf_moisture_content = mean(leaf_moisture_content),
+            degsec_100 = mean(degsec_100))
+
+##############################################################################
+# Back transformation and adjusting the slope and intercept
+#############################################################################
+tb_x1 <- 1
+
+tb_x1_transfromed <- tb_x1*sd(final_data$total_dry_mass_g) + mean(final_data$total_dry_mass_g)
+
+tb_x2 <- 3.5
+
+tb_x2_transformed <- tb_x2*sd(final_data$total_dry_mass_g) + mean(final_data$total_dry_mass_g)
+
+tb_y1 = coef(summary(canopy_traits_heat_release_model_mixed))[1] + 
+  coef(summary(canopy_traits_heat_release_model_mixed))[2]*tb_x1
+
+tb_y2 = coef(summary(canopy_traits_heat_release_model_mixed))[1] + 
+  coef(summary(canopy_traits_heat_release_model_mixed))[2]*tb_x2
+
+tb_slope <- (tb_y2 - tb_y1) / (tb_x2_transformed - tb_x1_transfromed)
+tb_intercept <- tb_y1 - tb_slope * tb_x1_transfromed
 
 
 ###############################################################################
@@ -60,74 +72,120 @@ degsec_by_group <- model_data %>%
 # data
 ###############################################################################
 
-
-total_dry_mass <- ggplot(best_degsec_plot_data, aes(total_dry_mass_g, degsec_100)) +
+total_dry_mass <- ggplot(best_degsec_plot_data, aes(total_dry_mass_g, degsec_100, color = display_name)) +
   geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
   geom_point(data = degsec_by_group, size = 4.5 , alpha = 1,
              shape = 16) +
   ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
   xlab("Total dry mass per 70 cm (g)")  + 
+  labs(color = "Species") +
   pubtheme +
-  theme(legend.position = "none") +
-  ## DWS: hard coding the values below is not ideal. This is fragile code.
+  scale_color_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                                "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "grey", "#4900FF",
+                                "#B600FF",  "#FF6D00", "#FF00DB")) +
+  scale_fill_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                               "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "#4900FF",
+                               "#B600FF",  "#FF6D00", "#FF00DB")) +
+  theme(legend.text = element_text(face = "italic")) +
   geom_abline(intercept = coef(summary(canopy_traits_heat_release_model_mixed))[1], 
               slope = coef(summary(canopy_traits_heat_release_model_mixed))[2], size = 1.5, color = "black")
 
-total_dry_mass_includingj <- ggplot(best_degsec_plot_data, aes(total_dry_mass_g, degsec_100)) +
-  geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
-  geom_point(data = degsec_by_group, size = 4.5 , alpha = 1,
-             shape = 16) +
-  geom_point(data = juniperus_data, size = 2.5, alpha = 0.5, color = "red") +
-  geom_point(data = mean_juniperus,  size = 4.5 , alpha = 1, color = "red") +
-  ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
-  xlab("Total dry mass per 70 cm (g)")  + 
-  pubtheme +
-  theme(legend.position = "none") +
-  ## DWS: hard coding the values below is not ideal. This is fragile code.
-  geom_abline(intercept = coef(summary(canopy_traits_heat_release_model_mixed))[1], 
-              slope = coef(summary(canopy_traits_heat_release_model_mixed))[2], size = 1.5, color = "black")
 
 ggsave("./results/total_dry_mass.pdf",
        plot = total_dry_mass, height = 180,
        width = 170, units = "mm", dpi = 300)
 
-ggsave("./results/total_dry_mass_includingj.pdf",
-       plot = total_dry_mass_includingj, height = 180,
+backtransformed_total_dry_mass <- ggplot(final_data, aes(total_dry_mass_g, degsec_100, color = display_name)) +
+  geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
+  geom_point(data = degsec_by_group_by_raw_data, size = 4.5 , alpha = 1,
+             shape = 16) +
+  ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
+  xlab("Total dry mass per 70 cm (g)")  + 
+  labs(color = "Species") +
+  pubtheme +
+  scale_color_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                                "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "grey", "#4900FF",
+                                "#B600FF",  "#FF6D00", "#FF00DB")) +
+  scale_fill_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                               "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "#4900FF",
+                               "#B600FF",  "#FF6D00", "#FF00DB")) +
+  theme(legend.text = element_text(face = "italic")) +
+  geom_abline(intercept = tb_intercept, 
+              slope = tb_slope, size = 1.5, color = "black")
+
+ggsave("./results/total_dry_mass_back_transformed.pdf",
+       plot = backtransformed_total_dry_mass, height = 180,
        width = 170, units = "mm", dpi = 300)
 
+##################################################################################################
+# leaf moisture content
+##################################################################################################
 
-leaf_moisture_content <- ggplot(best_degsec_plot_data, aes(leaf_moisture_content, degsec_100)) +
+leaf_moisture_content <- ggplot(model_data, aes(leaf_moisture_content, degsec_100, color = display_name)) +
   geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
   geom_point(data = degsec_by_group, size = 4.5 , alpha = 1,
              shape = 16) +
   ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
   xlab("Leaf moisture content (%)")  + 
+  labs(color = "Species") +
   pubtheme +
-  theme(legend.position = "none") +
+  scale_color_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                                "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "grey", "#4900FF",
+                                "#B600FF",  "#FF6D00", "#FF00DB")) +
+  scale_fill_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                               "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "#4900FF",
+                               "#B600FF",  "#FF6D00", "#FF00DB")) +
+  theme(legend.text = element_text(face = "italic")) +
   geom_abline(intercept = coef(summary(leaf_traits_heat_release_model_mixed))[1], 
               slope = coef(summary(leaf_traits_heat_release_model_mixed))[3], size = 1.5, color = "black") 
 
-
-leaf_moisture_content_includingj <- ggplot(best_degsec_plot_data, aes(leaf_moisture_content, degsec_100)) +
-  geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
-  geom_point(data = degsec_by_group, size = 4.5 , alpha = 1,
-             shape = 16) +
-  geom_point(data = juniperus_data, size = 2.5, alpha = 0.5, color = "red") +
-  geom_point(data = mean_juniperus,  size = 4.5 , alpha = 1, color = "red") +
-  ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
-  xlab("Leaf moisture content (%)")  + 
-  pubtheme +
-  theme(legend.position = "none") +
-  geom_abline(intercept = coef(summary(leaf_traits_heat_release_model_mixed))[1], 
-              slope = coef(summary(leaf_traits_heat_release_model_mixed))[3], size = 1.5, color = "black") 
 
 
 ggsave("./results/leaf_moisture_content.pdf",
        plot = leaf_moisture_content, height = 180,
        width = 170, units = "mm", dpi = 300)
 
-ggsave("./results/leaf_moisture_content_includingj.pdf",
-       plot = leaf_moisture_content_includingj, height = 180,
+##############################################################################
+# Back transformation and adjusting the slope and intercept for leaf moisture
+#############################################################################
+fmc_x1 <- 0
+
+fmc_x1_transfromed <- fmc_x1*sd(final_data$leaf_moisture_content) + mean(final_data$leaf_moisture_content)
+
+fmc_x2 <- 2
+
+fmc_x2_transformed <- fmc_x2*sd(final_data$leaf_moisture_content) + mean(final_data$leaf_moisture_content)
+
+fmc_y1 = coef(summary(leaf_traits_heat_release_model_mixed))[1] + 
+  coef(summary(leaf_traits_heat_release_model_mixed))[3]*fmc_x1
+
+fmc_y2 = coef(summary(leaf_traits_heat_release_model_mixed))[1] + 
+  coef(summary(leaf_traits_heat_release_model_mixed))[3]*fmc_x2
+
+fmc_slope <- (fmc_y2 - fmc_y1) / (fmc_x2_transformed - fmc_x1_transfromed)
+fmc_intercept <- fmc_y1 - fmc_slope * fmc_x1_transfromed
+
+leaf_moisture_content_back_transformed <- ggplot(final_data, aes(leaf_moisture_content, degsec_100, 
+                                                                 color = display_name)) +
+  geom_point(size = 2.5, alpha = 0.5, shape = 16) + 
+  geom_point(data = degsec_by_group_by_raw_data, size = 4.5 , alpha = 1,
+             shape = 16) +
+  ylab(expression(Temperature ~ integration ~ (degree~C %.% s ) )) +
+  xlab("Leaf moisture content (%)")  + 
+  labs(color = "Species") +
+  pubtheme +
+  scale_color_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                                "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "grey", "#4900FF",
+                                "#B600FF",  "#FF6D00", "#FF00DB")) +
+  scale_fill_manual(values = c("brown", "black", "#FFDB00", "#B6FF00", "#49FF00",
+                               "#FF0000", "#00FF92", "#00FFFF", "#0092FF", "#4900FF",
+                               "#B600FF",  "#FF6D00", "#FF00DB")) +
+  theme(legend.text = element_text(face = "italic")) +
+  geom_abline(intercept = fmc_intercept, 
+              slope = fmc_slope, size = 1.5, color = "black")
+
+ggsave("./results/leaf_moisture_content_back_transformed.pdf",
+       plot = leaf_moisture_content_back_transformed, height = 180,
        width = 170, units = "mm", dpi = 300)
 
 ############################################################################
@@ -176,8 +234,8 @@ pca_plot <- ggplot(pca_axis, aes(x = PC1, y = PC2)) +
     xend = PC1, yend = PC2),
     arrow = arrow(length = unit(4, "mm")),
     color = "black") +
-  labs(x = "Principle component 1",
-       y = "Principle component 2") +
+  labs(x = "Principle component 1 (75.9%)",
+       y = "Principle component 2 (10.1%)") +
   pubtheme +
   xlim(-0.1, 0.62) +
   geom_text(x = variable_loadings[1,1] + 0.08, y = variable_loadings[1,2] - 0.01,
