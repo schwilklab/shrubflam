c468f971d3bc2dfc30ebbb57b30fed063bb53c07
Azaj Mahmud <80069081+Azajmahmud@users.noreply.github.com>
2024-10-21 14:45:59 -0500

    removed unnecesary stuff from flam_pca.R
    Also removed analysis.R since now the analysis is in two different
    scripts with and without Juniper

--- scripts/flam_pca.R
+++ scripts/flam_pca.R
@@ -3,7 +3,6 @@
 # Shrub flammability project using summer 2022 data. PCA analysis uses prcomp()
 # for PCA and factoextra for figures
 
-library(factoextra)
 
 ###############################################################################
 # PCA analysis. Merging the hobo data with alldata_2022 by label data set to do
@@ -32,50 +31,6 @@ flam_loadings <- flam_pca_2022$rotation[ ,1:2]
 flam_loadings
 biplot(flam_pca_2022)
 
-##############################################################################
-## Scree plot, eigenvalue and variables info
-###############################################################################
-
-eig_val <- get_eigenvalue(flam_pca_2022) 
-eig_val # eignevalue for PC2 is 0.842
-variables_info <- get_pca_var(flam_pca_2022) # Variables information
-variables_info$coord[ ,1:2] # Coordinates of variables
-head(variables_info$contrib) # Contributions of variables
-
-##############################################################################
-## Plot based on cos2 values of variables
-###############################################################################
-
-# Will remove the plots and take them to results_2022.R later.
-
-var_contr_by_cos2 <- fviz_pca_var(flam_pca_2022, col.var = "cos2",
-                                           gradient.cols = c("#00AFBB","#E7B800","#FC4E07"),
-                                          repel = TRUE, col.circle = "white") +
-  xlab("Principle component 1") +
-  ylab("Principle component 2") +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_rect(),
-        plot.title = element_blank())
-
-
-
-
-###############################################################################
-## Contributions of variables in Principle components
-###############################################################################
-
-contributor_pc1_2022 <-  fviz_contrib(flam_pca_2022, choice = "var",
-                                 axes = 1,
-                                 fill = "lightgray",
-                                 color = "black") 
-
-
-
-contributor_pc2_2022 <- fviz_contrib(flam_pca_2022, choice = "var", 
-                                     axes = 2,
-                                     fill = "lightgray",
-                                     color = "black") 
 
 ###############################################################################
 # Assigning PC1 to pca_data_2022 and then merging with alldata_2022
@@ -93,12 +48,5 @@ dim(final_data)
 
 
 
-###############################################################################
-# Cleaning the environment
-###############################################################################
 
-rm("flam_loadings",
-   "eig_val", "variables_info" ,"contributor_pc1_2022",
-   "contributor_pc2_2022", 
-   "var_contr_by_cos2")
 
